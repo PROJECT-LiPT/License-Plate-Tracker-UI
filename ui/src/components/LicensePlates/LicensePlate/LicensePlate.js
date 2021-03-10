b07896ef69b2bd1c1a8ce7e0ccbc4265cd265dbf
Lê Bảo Anh <60655387+b1506704@users.noreply.github.com>
2021-03-11 01:32:23 +0700

    enhance design, fix bug

--- ui/src/components/LicensePlates/LicensePlate/LicensePlate.js
+++ ui/src/components/LicensePlates/LicensePlate/LicensePlate.js
@@ -1,30 +1,31 @@
 import React from 'react';
-import Draggable from 'react-draggable';
 
 import './LicensePlate.css';
 
 const LicensePlate = ({id, currentBase64}) => {
   const onCloseForm = () => {
-    //
+    //delete action
   }
-  return (
-    // <Draggable bounds="parent">
-    
-      <div id="menu" className="lp_container_menu">
-        <div className="title_bar">
-           {id}
+  if (id === 0) {
+    return null;
+  } else {
+      return (
+        <div id="menu" className="lp_container_menu">
+          <div className="title_bar">
+            {id}
+          {/* delete button */}
+          </div>
+          <div className="user_info">
+            <div> Owner: { currentBase64 ? <span> Mr. A </span> : <span> Loading...</span> } </div>
+            <div> Age: { currentBase64 ? <span> 28 </span> : <span> ...</span> } </div>
+            <div> Gender: { currentBase64 ? <span> Male </span> : <span> Loading...</span> }</div>
+            <div> Register location: { currentBase64 ? <span> Mars </span> : <span> Loading...</span> } </div>
+          </div>
+          <div className="lp_image_container">
+            { currentBase64 ? <img className="lp_image" alt="Loading..." src={`${currentBase64}`}/> : <span> Loading...</span> }
+          </div>
         </div>
-        <div className="user_info">
-          <div> Owner: { currentBase64 ? <span> Mr. A </span> : <span> Loading...</span> } </div>
-          <div> Age: { currentBase64 ? <span> 28 </span> : <span> ...</span> } </div>
-          <div> Gender: { currentBase64 ? <span> Male </span> : <span> Loading...</span> }</div>
-          <div> Register location: { currentBase64 ? <span> Mars </span> : <span> Loading...</span> } </div>
-        </div>
-        <div className="lp_image_container">
-          { currentBase64 ? <img className="lp_image" alt="Loading..." src={`${currentBase64}`}/> : <span> Loading...</span> }
-        </div>
-      </div>
-    // </Draggable>
-  );
+      );
+  }
 };
 export default LicensePlate;
